24387bd8632b1d0ed9349ffb8daba93f2f68a94a
stenio <stenioignacio@gmail.com>
2024-11-25 11:50:07 -0300

    v3 - modulo storage para AWS

--- storage/aws/provider.tf
+++ storage/aws/provider.tf
@@ -7,10 +7,7 @@ terraform {
   }
 }
 provider "aws" {
-  region                   = var.project_region
-  profile                  = "lab" #Essa tem que todos os recursos fora REDE
-  shared_config_files      = ["C:\\Users\\signacio\\.aws\\config"]
-  shared_credentials_files = ["C:\\Users\\signacio\\.aws\\credentials"]
+  region = var.project_region
   default_tags {
     tags = {
       Project     = "${var.project_name}"
